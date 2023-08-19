d6d4820cbcd62646a993338cc9f795e723407071
unnath123 <64188873+unnath123@users.noreply.github.com>
2023-08-19 23:45:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ var w = document.documentElement.clientWidth;
 var h = document.documentElement.clientHeight;
         
 // put the result into a h1 tag
- document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + "  Height: " + h + "</h1>";
+ document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and  Height: " + h + "</h1>";
 }
